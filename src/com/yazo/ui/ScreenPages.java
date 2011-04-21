d25fc0cc9e6002f955b826a18dc5c85b2d723c81
Jimmy Liang <solo123@21cn.com>
2011-04-21 13:25:24 +0800

    tt

--- src/com/yazo/ui/ScreenPages.java
+++ src/com/yazo/ui/ScreenPages.java
@@ -1,5 +1,19 @@
 package com.yazo.ui;
 
-public class ScreenPages {
+import javax.microedition.lcdui.Image;
+import com.yazo.books.*;
 
+public class ScreenPages {
+	BrowserContent[] contents;
+	Image[] screens;
+	
+	public ScreenPages(BrowserContent[] contents){
+		this.contents = contents;
+		screens = new Image[20];
+	}
+	
+	public void PreDraw(){
+		int i = 0;
+		
+	}
 }
