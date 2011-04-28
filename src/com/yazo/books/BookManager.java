60c3788d0370e48ac49e31f7d410d4bf90f0994c
unknown <hzbiao@.(none)>
2011-04-28 08:50:54 +0800

    1

--- src/com/yazo/books/BookManager.java
+++ src/com/yazo/books/BookManager.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
