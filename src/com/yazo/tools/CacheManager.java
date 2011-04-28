bfb22b3d5d5a1fd789d6d4b750f09ff420cf45ba
Jimmy Liang <solo123@21cn.com>
2011-04-28 16:32:53 +0800

    cache?

---
+++ src/com/yazo/tools/CacheManager.java
@@ -0,0 +1,14 @@
+package com.yazo.tools;
+
+import java.util.Hashtable;
+
+// TODO: add cache manager to advoid memory overflow
+public class CacheManager {
+	private Hashtable buffer;
+	public CacheManager(){
+		buffer = new Hashtable();
+	}
+	public void put(String key, Object val){
+		
+	}
+}
